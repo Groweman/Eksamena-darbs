cfd39962e0928c31f5e8332fba53df2c29df7192
Groweman <79845903+Groweman@users.noreply.github.com>
2021-06-17 03:46:04 +0300

    Tika izdomāts trešais jautājums

--- Eksamena-darbs.cpp
+++ Eksamena-darbs.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main(){
 	
-	int ans, score;
+	int ans, pareizi;
 	
 	score = 0;
 	
 	do{
 	cout<< "Do-while ir cikls"<< endl;
 	cout<< "1. Kas nostrada vismaz vienu reizi"<<endl;
-	cout<< "2. ar pecnosacijumu"<<endl;
-	cout<< "3. ar prieksnosacijumu"<<endl;
-	cout<< "4. nav cikls"<<endl;
+	cout<< "2. Ar pecnosacijumu"<<endl;
+	cout<< "3. Ar prieksnosacijumu"<<endl;
+	cout<< "4. Nav cikls"<<endl;
 	cin>>ans;
 	
 	if((ans < 1) || (ans > 4)){
@@ -37,17 +37,17 @@ int main(){
 	
 	do{
 	cout<< "Vai do-while cikla aiz nosacijuma liek ';'?"<< endl;
-	cout<< "1. Kas nostrada vismaz vienu reizi"<<endl;
-	cout<< "2. ar pecnosacijumu"<<endl;
-	cout<< "3. ar prieksnosacijumu"<<endl;
-	cout<< "4. nav cikls"<<endl;
+	cout<< "1. Ne"<<endl;
+	cout<< "2. Ja"<<endl;
+	cout<< "3. Ir obligati jaliek"<<endl;
+	cout<< "4. Ne jo While cikla to neliek"<<endl;
 	cin>>ans;
 	
 	if((ans < 1) || (ans > 4)){
 		cout<< "Izvelities vienu no iespejam"<<endl;
 	cout<<"*****************************"<<endl;
 	}else{
-		if(ans==1 || ans==2){
+		if(ans==2 || ans==3){
 
 	score +=1;
 			cout<<"Paldies par atbildi!"<<endl;
@@ -61,6 +61,69 @@ int main(){
 	
 	}while((ans < 1) || (ans > 4));
 	
+	
+	
+	do{
+	cout<< "Kurs no siem ir pareiza do-while sintakse?"<< endl;
+	cout<< "1. dowhile(nosacijums)\n {\n pazinojums \n}"<<endl;
+	cout<<"*****************************"<<endl;
+	cout<< "2. do{ \n pazinojums \n}while(nosacijums);"<<endl;
+	cout<<"*****************************"<<endl;
+	cout<< "3. do\n{ \n pazinojums \n}while(nosacijums)"<<endl;
+	cout<<"*****************************"<<endl;
+	cout<< "4. do\n{ \n pazinojums \n}while(nosacijums);"<<endl;
+	cout<<"*****************************"<<endl;
+	cin>>ans;
+	
+	if((ans < 1) || (ans > 4)){
+		cout<< "Izvelities vienu no iespejam"<<endl;
+	cout<<"*****************************"<<endl;
+	}else{
+		if(ans==2 || ans==4){
+
+	score +=1;
+			cout<<"Paldies par atbildi!"<<endl;
+			cout<<"*****************************"<<endl;
+		}else{
+			cout<<"Paldies par atbildi!" <<endl;
+			cout<<"*****************************"<<endl;
+		}
+		
+	}
+	
+	}while((ans < 1) || (ans > 4));
+	
+	
+	
+	do{
+	cout<< "Kads bus ši koda isa koda iznakums?\n int x=0;\n{\nx++;\ncout<<x<<'" "'''"<< endl;
+	cout<< "1. dowhile(nosacijums)\n {\n pazinojums \n}"<<endl;
+	cout<<"*****************************"<<endl;
+	cout<< "2. do{ \n pazinojums \n}while(nosacijums);"<<endl;
+	cout<<"*****************************"<<endl;
+	cout<< "3. do\n{ \n pazinojums \n}while(nosacijums)"<<endl;
+	cout<<"*****************************"<<endl;
+	cout<< "4. do\n{ \n pazinojums \n}while(nosacijums);"<<endl;
+	cout<<"*****************************"<<endl;
+	cin>>ans;
+	
+	if((ans < 1) || (ans > 4)){
+		cout<< "Izvelities vienu no iespejam"<<endl;
+	cout<<"*****************************"<<endl;
+	}else{
+		if(ans==2 || ans==4){
+
+	score +=1;
+			cout<<"Paldies par atbildi!"<<endl;
+			cout<<"*****************************"<<endl;
+		}else{
+			cout<<"Paldies par atbildi!" <<endl;
+			cout<<"*****************************"<<endl;
+		}
+		
+	}
+	
+	}while((ans < 1) || (ans > 4));
 
 
 return 0;
