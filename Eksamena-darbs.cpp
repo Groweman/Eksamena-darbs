6eb477fdc4473880c302942ec5d12f5d8e930eaf
Groweman <79845903+Groweman@users.noreply.github.com>
2021-06-17 03:28:00 +0300

    Tika samainīts otrais jautājums

--- Eksamena-darbs.cpp
+++ Eksamena-darbs.cpp
@@ -33,7 +33,35 @@ int main(){
 	
 	}while((ans < 1) || (ans > 4));
 	
-	cout<<score;
+	
+	
+	do{
+	cout<< "Vai do-while cikla aiz nosacijuma liek ';'?"<< endl;
+	cout<< "1. Kas nostrada vismaz vienu reizi"<<endl;
+	cout<< "2. ar pecnosacijumu"<<endl;
+	cout<< "3. ar prieksnosacijumu"<<endl;
+	cout<< "4. nav cikls"<<endl;
+	cin>>ans;
+	
+	if((ans < 1) || (ans > 4)){
+		cout<< "Izvelities vienu no iespejam"<<endl;
+	cout<<"*****************************"<<endl;
+	}else{
+		if(ans==1 || ans==2){
+
+	score +=1;
+			cout<<"Paldies par atbildi!"<<endl;
+			cout<<"*****************************"<<endl;
+		}else{
+			cout<<"Paldies par atbildi!" <<endl;
+			cout<<"*****************************"<<endl;
+		}
+		
+	}
+	
+	}while((ans < 1) || (ans > 4));
+	
+
 
 return 0;
 	}
